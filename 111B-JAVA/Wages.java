2e743dbe35e83e6f62fbc0ba9e3a99c02fb8fe39
Vinicius Santana <santanaviny@gmail.com>
2015-01-21 23:28:39 -0800

    Java studies & Assignment 1

---
+++ 111B-JAVA/Wages.java
@@ -0,0 +1,19 @@
+public class Wages
+{
+    public static void main(String[] args)
+    {
+        double regularWages;
+        double basePay = 25;
+        double regularHours = 40;
+        double overtimeWages;
+        double overtimePay = 37.5;
+        double overtimeHours = 10;
+        double totalWages;
+ 
+        regularWages = basePay * regularHours;
+        overtimeWages = overtimePay * overtimeHours;
+        totalWages = regularWages + overtimeWages; 
+        System.out.println("Wages for this week are $" +
+                                totalWages);
+    }
+}
