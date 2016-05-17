59c769bea5e0a36789fe3fad6468494d528036dd
Vinicius Santana <santanaviny@gmail.com>
2016-05-16 23:48:53 -0700

    Tic Tac Toe - Homework 6 - Fixes

--- cs111a/Homework/Homework-6/src/Main.java
+++ cs111a/Homework/Homework-6/src/Main.java
@@ -1,57 +1,6 @@
 /**
  * •••P6.25
- * Write a program that plays tic-tac-toe.
- * The tic-tac-toe game is played on a 3x3 grid as in the photo at right.
- * The game is played by two players, who take turns.
- * The first player marks moves with a circle, the second with a cross.
- * The player who has formed a horizontal, vertical,
- * or diagonal sequence of three marks wins.
- * Your program should draw the game board,
- * ask the user for the coordinates of the next mark,
- * change the players after every successful move,
- * and pronounce the winner.
-
-
- Here are additional instructions you must follow to receive full credit:
- (1) Do not use two-dimensional arrays of any kind.
- You must use a single-dimensional array of chars.
-
- (2) The board positions must be:
-
- 1 | 2 | 3
- ---+---+---
- 4 | 5 | 6
- ---+---+---
- 7 | 8 | 9
-
- (3) In addition to the main() method you should have these methods
- with the following headers:
-
- (a) void printBoard(char board[])
-
- (b) int nextPosition(char board[])
-
- // must validate that a correct position was entered
- // and that the board position is not already occupied
-
- (c) char updateMark(char mark)
-
- (d) void updateBoard(int pos, char mark, char board[])
-
- (e) boolean isWinner(char board[])
-
- Here's a possible algorithm:
- initialize the board
- initialize the mark
- print the board
- do {
- get valid position
- update board
- print the board
- update the mark
- determine if there is a winner
- } while (there is no winner);
- */
+**/
 
 import java.util.Scanner;
 
@@ -83,6 +32,7 @@ public class Main {
 
         System.out.println("To play, players must take turns and choose a position from 1 to 9 as per diagram bellow.");
         System.out.println("To win, a player must align 3 of his markers either horizontally, vertically or diagonally. \n");
+        System.out.println("Press 0 anytime to see the positions map. \n");
         printBoardPositions(boardState);
 
         System.out.println("Starting the game!! Good lucky!!");
@@ -103,6 +53,7 @@ public class Main {
 
     public static void printBoardPositions(char board[]){
         int pos = 0;
+
         for(int line = 0; line < 3; line++){
             for(int col = 0; col < 3; col++){
                 if(col == 0){
@@ -151,7 +102,7 @@ public class Main {
         Scanner myKeyboard = new Scanner(System.in);
 
         int playsCounter = 0;
-        String player = "Player 1";
+        String player = " ";
 
         for(int i = 0; i < board.length; i++){
             if(board[i] != ' '){
@@ -163,7 +114,9 @@ public class Main {
         System.out.print(player + " Choose a position: ");
         int pos = myKeyboard.nextInt();
 
-        while(pos < 0 || pos > 9 || board[pos - 1] != ' '){
+        if ( pos == 0 ){
+            printBoardPositions(board);
+        } else while(pos < 0 || pos > 9 || board[pos - 1] != ' '){
             System.out.print("You entered a taken or invalid position. Please try again: ");
             pos = myKeyboard.nextInt();
         }
@@ -175,12 +128,16 @@ public class Main {
     }
 
     public static void updateBoard(int pos, char mark, char board[]){
-        board[ pos - 1 ] = mark;
+        if ( pos != 0 ){
+            board[ pos - 1 ] = mark;
+        }
     }
 
     public static boolean isWinner(char board[]){
         boolean isWin = false;
-
+        int p1Plays = 0;
+        int p2Plays = 0;
+        String winner = " ";
         if(
             (board[0] == board[1] && board[1] == board[2] && board[0] != ' ') ||
             (board[3] == board[4] && board[4] == board[5] && board[3] != ' ') ||
@@ -192,9 +149,19 @@ public class Main {
             (board[2] == board[4] && board[4] == board[6] && board[2] != ' ')
            ){
             isWin = true;
-            System.out.println("* ******* * \n");
-            System.out.println("* Winner! * \n");
-            System.out.println("* ******* * \n");
+            for(int i = 0; i < board.length; i++){
+                if (board[i] == 'O'){
+                    p1Plays++;
+                }
+                if (board[i] == 'X'){
+                    p2Plays++;
+                }
+            }
+            winner = (p1Plays > p2Plays) ? "Player 1" : "Player 2";
+            System.out.println("* ************* *");
+            System.out.println("* "+ winner +" wins *");
+            System.out.println("* ************* *");
+
         }
 
         return isWin;
@@ -202,7 +169,7 @@ public class Main {
 
     public static boolean isTied(char board[]){
         int taken = 0;
-        boolean tied = false;
+        boolean isTied = false;
 
 
         for(int i = 0; i < board.length; i++){
@@ -211,17 +178,14 @@ public class Main {
             }
         }
         if (taken == 9 && isWinner(board) == false){
-            tied = true;
+            isTied = true;
             System.out.println("* ****** * \n");
             System.out.println("* Tied!! * \n");
             System.out.println("* ****** * \n");
         } else {
-            tied = false;
+            isTied = false;
         }
-
-
-
-        return tied;
+        return isTied;
     }
 
 }
